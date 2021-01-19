07a5fb82499303fbab6d1e7a53aea1600de11b45
KwonWJ <dnwns93@naver.com>
2021-01-19 15:32:29 +0900

    15:32

---
+++ final/src/main/java/co/team/apt/notice/service/impl/NoticeServiceImpl.java
@@ -0,0 +1,47 @@
+package co.team.apt.notice.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import co.team.apt.community.vo.BoardVo;
+import co.team.apt.notice.mapper.NoticeMapper;
+import co.team.apt.notice.service.NoticeService;
+
+@Service
+public class NoticeServiceImpl implements NoticeService{
+
+	@Autowired NoticeMapper dao;
+
+	@Override
+	public List<BoardVo> noticeList(BoardVo vo) {
+		// TODO Auto-generated method stub
+		return dao.noticeList(vo);
+	}
+
+	@Override
+	public BoardVo noticeOne(BoardVo vo) {
+		// TODO Auto-generated method stub
+		return dao.noticeOne(vo);
+	}
+
+	@Override
+	public int NoticeInsert(BoardVo vo) {
+		// TODO Auto-generated method stub
+		return dao.noticeInsert(vo);
+	}
+
+	@Override
+	public int NoticeUpdate(BoardVo vo) {
+		// TODO Auto-generated method stub
+		return dao.noticeUpdate(vo);
+	}
+
+	@Override
+	public int NoticeDelete(BoardVo vo) {
+		// TODO Auto-generated method stub
+		return dao.noticeDelete(vo);
+	}
+	
+}
