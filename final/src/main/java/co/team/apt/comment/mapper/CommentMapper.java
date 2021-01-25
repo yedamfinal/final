98af9b58c41d1acfe645c91faa862c2aa9f37ebb
KwonWJ <dnwns93@naver.com>
2021-01-25 19:32:12 +0900

    19:31

---
+++ final/src/main/java/co/team/apt/comment/mapper/CommentMapper.java
@@ -0,0 +1,17 @@
+package co.team.apt.comment.mapper;
+
+import java.util.List;
+
+import co.team.apt.common.vo.BcommentVo;
+
+
+public interface CommentMapper {
+	//댓글 조회
+	public List<BcommentVo> list(int bno) throws Exception;
+	//댓글 입력
+	public void write(BcommentVo vo);
+	//댓글 수정
+	public void modify(BcommentVo vo);
+	//댓글 삭제
+	public void delete(BcommentVo vo);
+}
