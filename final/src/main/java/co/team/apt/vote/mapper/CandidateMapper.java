c6cb7d3e33a0aa79e8472694ce38edf82646c290
kiw15 <woongs93@gmail.com>
2021-01-28 19:30:20 +0900

    귱 퇴근

---
+++ final/src/main/java/co/team/apt/vote/mapper/CandidateMapper.java
@@ -0,0 +1,23 @@
+package co.team.apt.vote.mapper;
+
+import java.util.List;
+
+import co.team.apt.common.vo.BoardVoteVo;
+import co.team.apt.common.vo.CandidateVo;
+import co.team.apt.common.vo.ResidentVo;
+
+public interface CandidateMapper {
+	List<CandidateVo> candidateList(CandidateVo vo);
+	
+	int candiInsert(CandidateVo vo);
+	int candiUpdate(CandidateVo vo);
+	int candiDelete(CandidateVo vo);
+
+	CandidateVo selectName(CandidateVo vo);
+	
+	
+	
+	
+	
+
+}
